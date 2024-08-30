b1455dcd847b0bd4bc9ddecb3fcb4d241934bcf3
ChaoticOfChaos <143908924+ChaoticOfChaos@users.noreply.github.com>
2024-08-30 19:26:33 -0300

    Adicionado Funções Após Login
    - Adicionado UI Simples de Linha de Comando
    - Adicionado Opção de Upload de Arquivos .xlsx

--- Graphics.py
+++ Graphics.py
@@ -3,8 +3,6 @@ import customtkinter as ctk
 log = False
 
 def interface(users, passwords):
-
-    log = False
     #Criando a tela de Login
     login_window = ctk.CTk()
     login_window.geometry('350x300')
@@ -16,24 +14,22 @@ def interface(users, passwords):
     #Configurando as entradas
 
     user_entry = ctk.CTkEntry(login_window,
-                        placeholder_text="Usuário")
-    user_entry.pack(pady=20)
+                        placeholder_text="Usuário"
+                        ).pack()
     password_entry = ctk.CTkEntry(login_window,
                             placeholder_text="Senha",
                             show='*'
-                            )
-    password_entry.pack(pady=10)
+                            ).pack(pady=20)
     def logar():
-        nonlocal log, users, passwords
+        global log
         log = True
-        users = user_entry.get()
-        passwords = password_entry.get()
         login_window.destroy()
 
     btn_login = ctk.CTkButton(login_window,
                               text='Logar',
-                              command=logar)
-    btn_login.pack(pady=20)
+                              command=logar).pack(pady=20)
     login_window.mainloop()
     if log:
+        users = user_entry.get()
+        passwords = password_entry.get()
         return users, passwords
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pandas as pd
-import Graphics as G
+import os
 
+print('Bem Vindo ao gerenciador'.center(20))
 
 #sitema de login
-db = pd.read_csv('./data/data.csv')
+db = pd.read_csv('./data/logins.csv')
 while True:
     try:
         r = int(input('O que deseja fazer?\nRegistrar - 1 Logar - 2 '))
@@ -31,13 +32,16 @@ while True:
                     db.loc[index, 'email'] = email
                     db.loc[index, 'senha'] = senha
 
-                    db.to_csv('./data/data.csv', index=False)
+                    db.to_csv('./data/logins.csv', index=False)
                 else:
                     print('Erro. Email já cadastrado')
             else:
                 print('Erro. Não é permitido caracteres especiais como : ";", ">", "<", ";", ":"')
         case 2:
-            email, senha = G.interface('', '')
+            # Fazer login pelo nome não é uma prática segura
+            email = input('Digite o email ')
+            senha = input('Digite a senha do usuário ')
+            
             login = False
             if db['email'].isin([email]).any():
                 pssword = db.loc[db['email'] == email, 'senha'].values[0]
@@ -51,6 +55,49 @@ while True:
                 print('Email não encontrado')
         
             if login:
-                break
+                # Incrementar a UI aqui
+                print('Opções:')
+                print('1 - Upload de Arquivos Excel')
+                print('0 - Sair')
+                print('? - Ajuda')
+
+                while True:
+                    userInput = input(' -> ')
+
+                    match userInput:
+                        case '1':
+                            xlsx = input('Caminho do Arquivo .xlsx -> ')
+
+                            if os.path.exists(xlsx):
+                                excel = pd.read_excel(xlsx)
+                                dataFrame = pd.read_csv('./data/data.csv')
+
+                                indexData = len(dataFrame['email'])
+
+                                for i in range(len(excel)):
+                                    dataFrame.loc[indexData, 'email'] = email
+                                    dataFrame.loc[indexData, 'vendas'] = excel.loc[i, 'vendas']
+                                    dataFrame.loc[indexData, 'item'] = excel.loc[i, 'item']
+                                    dataFrame.loc[indexData, 'comissao'] = excel.loc[i, 'comissao']
+
+                                    indexData += 1
+
+                                dataFrame.to_csv('./data/data.csv', index=False)
+
+                            else:
+                                print('Arquivo Não Existe')
+
+                        case '0':
+                            break
+
+                        case '?':
+                            print('1 - Upload de Excel')
+                            print('0 - Sair')
+                            print('? - Ajuda')
+
+                        case _:
+                            print('Opção Invalida')
+
+                        # A UI Vai Até Aqui
             else:
                 print('Não entrado, Verifique se o email esta correto ou registre-se')
