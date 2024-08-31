a2ca1c9deb3a6ff656f2ae812b40ba59699399bf
ySpac3 <pedrohenriquef243@gmail.com>
2024-08-30 22:41:42 -0300

    Add files via upload

--- Graphics.py
+++ Graphics.py
@@ -2,38 +2,160 @@ import customtkinter as ctk
 
 log = False
 
-def interface(users, passwords):
-
-    log = False
-    #Criando a tela de Login
-    login_window = ctk.CTk()
-    login_window.geometry('350x300')
-    login_window.resizable(width=False, height=False)
 
 
-    # Configurando a tela inicial
-    login_text = ctk.CTkLabel(login_window, text='LOGIN', font=('Arial', 20)).pack(pady=10)
-    #Configurando as entradas
 
-    user_entry = ctk.CTkEntry(login_window,
-                        placeholder_text="Usuário")
-    user_entry.pack(pady=20)
-    password_entry = ctk.CTkEntry(login_window,
-                            placeholder_text="Senha",
-                            show='*'
-                            )
-    password_entry.pack(pady=10)
-    def logar():
-        nonlocal log, users, passwords
-        log = True
-        users = user_entry.get()
-        passwords = password_entry.get()
-        login_window.destroy()
+class login_UI:
+    def __init__(self):
+        self.users = None
+        self.passwords = None
+        self.email = None
 
-    btn_login = ctk.CTkButton(login_window,
-                              text='Logar',
-                              command=logar)
-    btn_login.pack(pady=20)
-    login_window.mainloop()
-    if log:
-        return users, passwords
+    def inicial_login_ui(self):
+        #declarando variaveis
+        log = False
+        reg = False
+        #declarando Funções
+        def logar():
+            nonlocal log  # Variavel Verificando se vai logar
+            log = True
+            self.email = user_entry.get()
+            self.passwords = password_entry.get()
+            login_window.destroy()
+
+        def registrar():
+            nonlocal reg
+            reg = True
+            login_window.destroy()
+
+
+        # Criando a tela de Login
+        login_window = ctk.CTk()
+        login_window.geometry('350x300')
+        login_window.resizable(width=False, height=False)
+
+        # Configurando a tela inicial
+        login_text = ctk.CTkLabel(login_window, text='LOGIN', font=('Arial', 20)).pack(pady=10)
+        # Configurando as entradas
+
+        user_entry = ctk.CTkEntry(login_window,
+                                  placeholder_text="Email")
+        user_entry.pack(pady=20)
+        password_entry = ctk.CTkEntry(login_window,
+                                      placeholder_text="Senha",
+                                      show='*'
+                                      )
+        password_entry.pack(pady=10)
+
+        btn_login = ctk.CTkButton(login_window,
+                                  text='Logar',
+                                  command=logar)
+        btn_login.pack(pady=20)
+
+        btn_register = ctk.CTkButton(login_window,
+                                     text='Registrar',
+                                     command=registrar,
+                                     fg_color='Purple')
+        btn_register.pack(pady=10)
+
+        login_window.mainloop()
+        if log:
+            return self.email, self.passwords
+        if reg:
+            return self.email, self.passwords
+    def incorrect_login(self):
+        #declarando variaveis
+        log = False
+        reg = False
+        #declarando Funções
+        def logar():
+            nonlocal log
+            log = True
+            self.email = user_entry.get()
+            self.passwords = password_entry.get()
+            login_window.destroy()
+        def registrar():
+            nonlocal reg
+            reg = True
+            login_window.destroy()
+
+        # Criando a tela de Login
+        login_window = ctk.CTk()
+        login_window.geometry('350x300')
+        login_window.resizable(width=False, height=False)
+
+        # Configurando a tela inicial
+        login_text = ctk.CTkLabel(login_window, text='LOGIN', font=('Arial', 20)).pack(pady=10)
+        # Configurando as entradas
+
+        user_entry = ctk.CTkEntry(login_window,
+                                  placeholder_text="Email",
+                                  border_color='red')
+        user_entry.pack(pady=20)
+        password_entry = ctk.CTkEntry(login_window,
+                                      placeholder_text="Senha",
+                                      show='*',
+                                      border_color='red'
+                                      )
+        password_entry.pack(pady=10)
+
+        btn_login = ctk.CTkButton(login_window,
+                                  text='Logar',
+                                  command=logar)
+        btn_login.pack(pady=20)
+
+        btn_register = ctk.CTkButton(login_window,
+                                     text='Registrar',
+                                     command=registrar,
+                                     fg_color='Purple')
+        btn_register.pack(pady=10)
+        login_window.mainloop()
+        if log:
+            return self.email, self.passwords
+        if reg:
+            return self.email, self.passwords
+
+    def register(self):
+        #Declarando Variaveis
+        reg = False
+        #Declarando Funções
+        def registrar():
+            nonlocal reg
+            reg = True
+            self.users = user_entry.get()
+            self.passwords = password_entry.get()
+            self.email = email_entry.get()
+            register_window.destroy()
+
+        # Criando a tela de Login
+        register_window = ctk.CTk()
+        register_window.geometry('350x300')
+        register_window.resizable(width=False, height=False)
+
+        # Configurando a tela inicial
+        login_text = ctk.CTkLabel(register_window, text='Registrar', font=('Arial', 20)).pack(pady=10)
+        # Configurando as entradas
+
+        user_entry = ctk.CTkEntry(register_window,
+                                  placeholder_text="Usuário")
+        user_entry.pack(pady=10)
+
+        password_entry = ctk.CTkEntry(register_window,
+                                      placeholder_text="Senha",
+                                      show='*'
+                                      )
+        password_entry.pack(pady=10)
+
+        email_entry = ctk.CTkEntry(register_window,
+                                   placeholder_text="Email",
+                                   )
+        email_entry.pack(pady=10)
+
+        btn_registrar = ctk.CTkButton(register_window,
+                                      text='Registrar',
+                                      fg_color='Purple',
+                                      command=registrar)
+        btn_registrar.pack(pady=10)
+        register_window.mainloop()
+        if reg:
+            return self.users, self.passwords, self.email
--- main.py
+++ main.py
@@ -1,103 +1,97 @@
 import pandas as pd
 import os
+import Graphics as G
 
 print('Bem Vindo ao gerenciador'.center(20))
 
 #sitema de login
 db = pd.read_csv('./data/logins.csv')
-while True:
-    try:
-        r = int(input('O que deseja fazer?\nRegistrar - 1 Logar - 2 '))
-    except TypeError: # Adicionei o Except Correto
-        print('Comando invalido tente novamente')
+# aqui ta a função da UI
 
-    match r:
-        case 1:
-            # input() já retorna str
-            chars = [' ', ';', '>', '<', ';', ':'] # Caracteres especiais, que vão ajudar a tratar ataques de SQLInjection
-            nome = input('Digite seu nome ')
-            senha = input('Digite sua senha ')
-            email = input('Digite seu email ')
-            
-            # Sistema básico contra SQLInjection
-            erroChar = False
-            for inp in [nome, senha, email]:
-                if any(char in inp for char in chars):
-                    erroChar = True
+# Começo da tela de login aqui
+email, senha = G.login_UI().inicial_login_ui()
 
-            if not erroChar:
-                if not db['email'].isin([email]).any():
-                    index = len(db['email'])
-                    db.loc[index, 'nome'] = nome
-                    db.loc[index, 'email'] = email
-                    db.loc[index, 'senha'] = senha
+login = False
+if db['email'].isin([email]).any():
+    pssword = db.loc[db['email'] == email, 'senha'].values[0]
+    if pssword == senha:
+        login = True
 
-                    db.to_csv('./data/logins.csv', index=False)
+    else:
+        if email != None or senha != None:
+            email, senha = G.login_UI().incorrect_login()
+
+else:
+    if email != None or senha != None:
+        email, senha = G.login_UI().incorrect_login()
+
+if login:
+    # Incrementar a UI aqui
+    print('Opções:')
+    print('1 - Upload de Arquivos Excel')
+    print('0 - Sair')
+    print('? - Ajuda')
+
+    while True:
+        userInput = input(' -> ')
+
+        match userInput:
+            case '1':
+                xlsx = input('Caminho do Arquivo .xlsx -> ')
+
+                if os.path.exists(xlsx):
+                    excel = pd.read_excel(xlsx)
+                    dataFrame = pd.read_csv('./data/data.csv')
+
+                    indexData = len(dataFrame['email'])
+
+                    for i in range(len(excel)):
+                        dataFrame.loc[indexData, 'email'] = email
+                        dataFrame.loc[indexData, 'vendas'] = excel.loc[i, 'vendas']
+                        dataFrame.loc[indexData, 'item'] = excel.loc[i, 'item']
+                        dataFrame.loc[indexData, 'comissao'] = excel.loc[i, 'comissao']
+
+                        indexData += 1
+
+                    dataFrame.to_csv('./data/data.csv', index=False)
+
                 else:
-                    print('Erro. Email já cadastrado')
-            else:
-                print('Erro. Não é permitido caracteres especiais como : ";", ">", "<", ";", ":"')
-        case 2:
-            # Fazer login pelo nome não é uma prática segura
-            email = input('Digite o email ')
-            senha = input('Digite a senha do usuário ')
-            
-            login = False
-            if db['email'].isin([email]).any():
-                pssword = db.loc[db['email'] == email, 'senha'].values[0]
-                if pssword == senha:
-                    login = True
+                    print('Arquivo Não Existe')
 
-                else:
-                    print('Senha Incorreta')
-            
-            else:
-                print('Email não encontrado')
-        
-            if login:
-                # Incrementar a UI aqui
-                print('Opções:')
-                print('1 - Upload de Arquivos Excel')
+            case '0':
+                break
+
+            case '?':
+                print('1 - Upload de Excel')
                 print('0 - Sair')
                 print('? - Ajuda')
 
-                while True:
-                    userInput = input(' -> ')
+            case _:
+                print('Opção Invalida')
 
-                    match userInput:
-                        case '1':
-                            xlsx = input('Caminho do Arquivo .xlsx -> ')
+            # A UI Vai Até Aqui
 
-                            if os.path.exists(xlsx):
-                                excel = pd.read_excel(xlsx)
-                                dataFrame = pd.read_csv('./data/data.csv')
 
-                                indexData = len(dataFrame['email'])
+#Aqui começa a tela de registro
 
-                                for i in range(len(excel)):
-                                    dataFrame.loc[indexData, 'email'] = email
-                                    dataFrame.loc[indexData, 'vendas'] = excel.loc[i, 'vendas']
-                                    dataFrame.loc[indexData, 'item'] = excel.loc[i, 'item']
-                                    dataFrame.loc[indexData, 'comissao'] = excel.loc[i, 'comissao']
+chars = [' ', ';', '>', '<', ';', ':'] # Caracteres especiais, que vão ajudar a tratar ataques de SQLInjection
+nome, senha, email = G.login_UI().register()
 
-                                    indexData += 1
+# Sistema básico contra SQLInjection
+erroChar = False
+for inp in [nome, senha, email]:
+    if any(char in inp for char in chars):
+        erroChar = True
 
-                                dataFrame.to_csv('./data/data.csv', index=False)
+if not erroChar:
+    if not db['email'].isin([email]).any():
+        index = len(db['email'])
+        db.loc[index, 'nome'] = nome
+        db.loc[index, 'email'] = email
+        db.loc[index, 'senha'] = senha
 
-                            else:
-                                print('Arquivo Não Existe')
-
-                        case '0':
-                            break
-
-                        case '?':
-                            print('1 - Upload de Excel')
-                            print('0 - Sair')
-                            print('? - Ajuda')
-
-                        case _:
-                            print('Opção Invalida')
-
-                        # A UI Vai Até Aqui
-            else:
-                print('Não entrado, Verifique se o email esta correto ou registre-se')
+        db.to_csv('./data/logins.csv', index=False)
+    else:
+        print('Erro. Email já cadastrado')
+else:
+    print('Erro. Não é permitido caracteres especiais como : ";", ">", "<", ";", ":"')
