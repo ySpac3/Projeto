897be7f82da3174dcdb09be2a03c9b75a4f01b43
ChaoticOfChaos <143908924+ChaoticOfChaos@users.noreply.github.com>
2024-09-04 13:12:35 -0300

    Novas Funções
    - Adicionada Função Para Separar os Vendedores Por Login
    - Adicionada Função Para Buscar por Vendedores

--- Functions.py
+++ Functions.py
@@ -1,4 +1,3 @@
-
 import os
 import pandas as pd
 
@@ -32,3 +31,22 @@ def upload_data(path):
 
     else:
         print('Arquivo Não Existe')
+
+# Favor usar no login, o nome do usuário
+# Favor Invocar essa função ao se registrar
+def criarVendedores(login: str, nomeDoVendedor: str) -> None:
+    # Trabalhar com pastas que usem espaço é foda
+    login = login.replace(' ', '-')
+    nomeDoVendedor = nomeDoVendedor.replace(' ', '-')
+    os.mkdir(f'./vendedores/{login}/{nomeDoVendedor}')
+    with open(f'./vendedores/{login}/{nomeDoVendedor}', 'w') as tb:
+        tb.write('vendas,comissao,valor')
+
+# Função que retorn a tabela do vendedor
+def queryVendedores(login: str, nomeDoVendedor: str) -> pd.DataFrame:
+    login = login.replace(' ', '-')
+    nomeDoVendedor = nomeDoVendedor.replace(' ', '-')
+    if os.path.exists(f'./vendedores/{login}/{nomeDoVendedor}/{nomeDoVendedor}-tab.csv'):
+        return pd.read_csv(f'./vendedores/{login}/{nomeDoVendedor}/{nomeDoVendedor}-tab.csv')
+    else:
+        return pd.DataFrame()
